1453f56b9fa5881cd64c29f2ef598c610fff6dc2
lalohuis <eduardohuertaislas96@gmail.com>
2024-09-28 23:29:32 -0600

    Add files via upload

---
+++ filtro.js
@@ -0,0 +1,56 @@
+const productos = [
+  {nombre: "Zapato negro", tipo: "zapato", color: "negro", img: "./taco-negro.jpg"},
+  {nombre: "Zapato azul", tipo: "zapato", color: "azul", img: "./taco-azul.jpg"},
+  {nombre: "Bota negra", tipo: "bota", color: "negro", img: "./bota-negra.jpg"},
+  {nombre: "Bota azul", tipo: "bota", color: "azul", img: "./bota-azul.jpg"},
+  {nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg"}
+]
+
+const li = document.getElementById("lista-de-productos"); //Se cambia getElementsByName por getElementById
+const $i = document.querySelector('.input');
+
+
+for (let i = 0; i < productos.length; i++) {
+  var d = document.createElement("div")
+  d.classList.add("producto")
+
+  var ti = document.createElement("p")
+  ti.classList.add("titulo")
+  ti.textContent = productos[i].nombre
+  
+  var imagen = document.createElement("img");
+  imagen.setAttribute('src', productos[i].img);
+
+  d.appendChild(ti)
+  d.appendChild(imagen)
+
+  li.appendChild(d)
+}
+ 
+const botonDeFiltro = document.getElementById("filter-btn"); //se agrega el getElementById para poder mandar a llamar el botón
+
+botonDeFiltro.onclick = function() {
+  li.innerHTML = '';
+
+  const texto = $i.value.toLowerCase(); 
+  const productosFiltrados = productos.filter(item => 
+    item.tipo.includes(texto) || item.color.includes(texto)
+  );
+
+  for (let i = 0; i < productosFiltrados.length; i++) {
+    var d = document.createElement("div")
+    d.classList.add("producto")
+  
+    var ti = document.createElement("p")
+    ti.classList.add("titulo")
+    ti.textContent = productosFiltrados[i].nombre
+    
+    var imagen = document.createElement("img");
+    imagen.setAttribute('src', productosFiltrados[i].img);
+  
+    d.appendChild(ti)
+    d.appendChild(imagen)
+  
+    li.appendChild(d)
+  }
+}
